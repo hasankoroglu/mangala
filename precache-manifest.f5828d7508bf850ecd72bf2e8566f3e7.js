6a7c9612887de18dfb02da58ea17b1f104ae5603
hasankoroglu <hasan@hasankoroglu.com>
2019-07-31 15:29:52 +0300

    Updates

---
+++ precache-manifest.f5828d7508bf850ecd72bf2e8566f3e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153968dd4f47d44e84217885904dbfef",
+    "url": "/mangala/index.html"
+  },
+  {
+    "revision": "47892722792cdd4f2071",
+    "url": "/mangala/static/css/main.45a36158.chunk.css"
+  },
+  {
+    "revision": "fb347551d423f8bd33de",
+    "url": "/mangala/static/js/2.f5c2ba15.chunk.js"
+  },
+  {
+    "revision": "47892722792cdd4f2071",
+    "url": "/mangala/static/js/main.e64ea86f.chunk.js"
+  },
+  {
+    "revision": "b9a7fc70e57be47cd6ac",
+    "url": "/mangala/static/js/runtime~main.b35d9d12.js"
+  },
+  {
+    "revision": "732ee5d212f35edd3e06ab9d29d7b918",
+    "url": "/mangala/static/media/wood.732ee5d2.jpg"
+  }
+]);
